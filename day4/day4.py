a0974ae30a85b90c0b0c6845546658950c65b505
Zanilicious <joshua0906@gmail.com>
2024-07-23 23:40:10 +0200

    Finished day4 pt1

--- day4/day4.py
+++ day4/day4.py
@@ -31,6 +31,33 @@ Card 6 has no winning numbers, so it is worth no points.
 So, in this example, the Elf's pile of scratchcards is worth 13 points.
 
 Take a seat in the large pile of colorful cards. How many points are they worth in total?
+--- Part Two ---
+Just as you're about to report your findings to the Elf, one of you realizes that the rules have actually been printed on the back of every card this whole time.
+
+There's no such thing as "points". Instead, scratchcards only cause you to win more scratchcards equal to the number of winning numbers you have.
+
+Specifically, you win copies of the scratchcards below the winning card equal to the number of matches. So, if card 10 were to have 5 matching numbers, you would win one copy each of cards 11, 12, 13, 14, and 15.
+
+Copies of scratchcards are scored like normal scratchcards and have the same card number as the card they copied. So, if you win a copy of card 10 and it has 5 matching numbers, it would then win a copy of the same cards that the original card 10 won: cards 11, 12, 13, 14, and 15. This process repeats until none of the copies cause you to win any more cards. (Cards will never make you copy a card past the end of the table.)
+
+This time, the above example goes differently:
+
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+Card 1 has four matching numbers, so you win one copy each of the next four cards: cards 2, 3, 4, and 5.
+Your original card 2 has two matching numbers, so you win one copy each of cards 3 and 4.
+Your copy of card 2 also wins one copy each of cards 3 and 4.
+Your four instances of card 3 (one original and three copies) have two matching numbers, so you win four copies each of cards 4 and 5.
+Your eight instances of card 4 (one original and seven copies) have one matching number, so you win eight copies of card 5.
+Your fourteen instances of card 5 (one original and thirteen copies) have no matching numbers and win no more cards.
+Your one instance of card 6 (one original) has no matching numbers and wins no more cards.
+Once all of the originals and copies have been processed, you end up with 1 instance of card 1, 2 instances of card 2, 4 instances of card 3, 8 instances of card 4, 14 instances of card 5, and 1 instance of card 6. In total, this example pile of scratchcards causes you to ultimately have 30 scratchcards!
+
+Process all of the original and copied scratchcards until no more scratchcards are won. Including the original set of scratchcards, how many total scratchcards do you end up with?
 """
 # METHOD
 # Open file, load all lines --> list, close file
@@ -76,16 +103,33 @@ def close_file(file):
 def split_line(line):
     line = re.split(":|\||\n", line)
     # Removes empty char at end of list
-    line.pop()
+    if line[2] == "":
+        line.pop()
 
-    # Split the list into further constituents
-    for i in range(0, len(line)):
-        line[i] = line[i].split(" ")
+    # Handle card ID
+    card_id = ""
+    for i in line[0]:
+        if i in "0123456789":
+            card_id += i
+
+    # Handle winning numbers
+    winning_numbers = []
+    for i in line[1].split(" "):
+        if i != "":
+            winning_numbers.append(i)
     
+    # Handle my numbers
+    my_numbers = []
+    for i in line[2].split(" "):
+        if i != "":
+            my_numbers.append(i)
+            
+    temp_dict = {"id" : card_id,
+                 "winning_numbers" : winning_numbers,
+                 "my_numbers" : my_numbers}
     
-
-
-
+    return temp_dict
+    
 
 # Parse lines into list that contains dictionaries
 # Keys: 
@@ -102,19 +146,48 @@ def parse_lines(lines):
     
     # Loop through all lines (which were loaded from input file)
     for line in lines:
-        temp_dict = {}
+        temp_dict = split_line(line)
+        dict_list.append(temp_dict)
         
-
-
     return dict_list
 
+# Helper function
+# Calculate score based on how many of my numbers can be found in the winning numbers
+# Follows formula as outlined in text
+def calc_score(dictionary):
+    score = 0
+
+    for i in dictionary["my_numbers"]:
+        if i in dictionary["winning_numbers"]:
+            if score == 0:
+                score += 1
+            else:
+                score *= 2
+    
+    return score
+
+# Calculates the total score
+def total_score(dict_list):
+    total_score = 0
+
+    for i in dict_list:
+        total_score += calc_score(i)
+
+    return total_score
+
+
 # MAIN
+def main():
+    f = load_file("input")
 
-# TEST BED
-f = load_file("input")
+    lines = load_lines(f)
 
-lines = load_lines(f)
+    close_file(f)
 
-close_file(f)
+    dict_list = parse_lines(lines)
+    
+    score = total_score(dict_list)
 
-print(lines)
+    print("The total score for part 1 is:", score)
+
+main()
